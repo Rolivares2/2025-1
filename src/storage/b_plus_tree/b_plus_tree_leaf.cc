7abb92f8345ab812b79b7752e1e6347196e2a9be
rolivares2 <rolivares2@uc.cl>
2025-04-28 19:33:30 -0400

    Se completo el problema 2

--- src/storage/b_plus_tree/b_plus_tree_leaf.cc
+++ src/storage/b_plus_tree/b_plus_tree_leaf.cc
@@ -31,11 +31,46 @@ std::unique_ptr<BPlusTreeSplit> BPlusTreeLeaf::insert_record(const BPlusTreeReco
   }
 
   if (record_count < max_records) {
-    // TODO: Problema 1
+    // TODO: Testear Problema 1
+    const auto key = search_index(record);
+    for (auto i = record_count; i >= key; i--) {
+      set_record(i+1, get_record(i));
+    }
+    set_record(key, record);
+    set_record_count(record_count++);
+    return nullptr
   } else {
     // TODO: Problema 2
+    // 1. Encontrar el índice donde el record se inserta:
+    const auto key = search_index(record);
+    // 2. Definir secuencia
+    vector<BPlusTreeRecord> record_seq;
+    for (int32_t i = 0; i < record_count; i++){
+      // Se construye la secuencia hasta el record a insertar
+      record_seq.push_back(get_record(i));
+    }
+    // Se inserta el record dado en la función
+    record_seq.insert(record_seq.begin() + key, record);
+    // 3. Se crea una nueva hoja con el constructor BPlusTreeLeaf
+    BPlusTreeLeaf new_leaf = BPlusTreeLeaf(bpt);
+    // 4. Definición del Middle index.
+    const auto middle = (max_records+1)/2;
+    // 5. Actualización del N de la hoja original
+    set_record_count(middle);
+
+    for (int32_t i = middle; i < record_seq.size(); i++) {
+      // Inserción de los records a la nueva hoja
+      new_leaf.set_record(i - middle, record_seq[i]);
+    }
+    new_leaf.set_record_count((max_records/2)+1);
+    new_leaf.set_next_page_number(get_next_page_number());
+    // Actualización del next_ptr de la página original
+    set_next_page_number(new_leaf.page.get_page_number());
+    // 7. Retornar BPlusTreeSplit
+    return BPlusTreeSplit(new_leaf.get_record(0).encoded_key, new_leaf.page.get_page_number());
   }
 }
+
 
 void BPlusTreeLeaf::delete_record(const BPlusTreeRecord& record) {
   const auto record_count = get_record_count();
@@ -44,7 +79,7 @@ void BPlusTreeLeaf::delete_record(const BPlusTreeRecord& record) {
   }
   // TODO: Bonus
 }
-
+push_back
 int32_t BPlusTreeLeaf::get_record_count() const {
   return page.read_int32(OFFSET_RECORD_COUNT);
 }
